a962ed8c3d2cbe3b8db23401da435eb0657ce90c
GitHubestudio <176661583+GitHubestudio@users.noreply.github.com>
2025-05-21 18:35:29 -0300

    agrego clase gestion

---
+++ TP7-Grupo18-Programacion/Clases/GestionSucursales.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace TP7_Grupo18_Programacion.Clases
+{
+    public class GestionSucursales
+    {
+        //CONSTRCUTOR
+        public GestionSucursales()
+        {
+
+        }
+
+        //METODOS
+        private DataTable ObtenerTablaProductos(string nombreTabla, string consultaSql)
+        {
+            // Creamos un nuevo contenedor de datos
+            DataSet dataSet = new DataSet();
+
+            // Instanciamos nuestra clase de acceso a datos para obtener el adaptador
+            Conexion accesoDatos = new Conexion();
+
+            // Obtenemos un adaptador SQL configurado con la consulta y la conexión
+            SqlDataAdapter sqlDataAdapter = accesoDatos.ObtenerAdaptador(consultaSql);
+
+            // Ejecutamos la consulta y llenamos el DataSet con los resultados,
+            // usando como nombre interno el que se pasó como parámetro (nombreTabla)
+            sqlDataAdapter.Fill(dataSet, nombreTabla);
+
+            // Devolvemos la tabla del DataSet que acabamos de llenar
+            return dataSet.Tables[nombreTabla];
+        }
+
+        // Obtiene todas las filas de la tabla 'Sucursal' desde la base de datos
+        public DataTable ObtenerTodosLosProductos()
+        {
+            // Definimos la consulta SQL para traer todas las columnas y filas de la tabla 'Sucursal'
+            string consultaSql = "SELECT * FROM Sucursal";
+
+            return ObtenerTablaProductos("Sucursal", consultaSql);
+        }
+
+
+    }
+}
