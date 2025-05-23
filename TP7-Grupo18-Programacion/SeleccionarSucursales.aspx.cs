b2a1bd771589e8dee78074b0cdb95c25ab318df6
Nicolas-Sosa1 <nicolassosaesteban2003@gmail.com>
2025-05-22 21:48:20 -0300

    CreateTabla

--- TP7-Grupo18-Programacion/SeleccionarSucursales.aspx.cs
+++ TP7-Grupo18-Programacion/SeleccionarSucursales.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -38,6 +39,39 @@ namespace TP7_Grupo18_Programacion
             dlProvincias.DataSource = gestion.ObtenerProvincias();
             dlProvincias.DataBind();
         }
+
+        private DataTable CrearTabla()
+        {
+            DataTable dataTable = new DataTable();
+
+            DataColumn dataColumn;
+
+            dataColumn = new DataColumn("Id_Sucursal", typeof(string));
+            dataTable.Columns.Add(dataColumn);
+
+            dataColumn = new DataColumn("NombreSucursal", typeof(string));
+            dataTable.Columns.Add(dataColumn);
+
+            dataColumn = new DataColumn("DescripcionSucursal", typeof(string));
+            dataTable.Columns.Add(dataColumn);
+
+            return dataTable;
+        }
+
+        private DataTable AgregarFila(DataTable dataTable, string idSucursal, string nombre, string descripcion)
+        {
+            DataRow dataRow = dataTable.NewRow();
+
+            dataRow["Id_Sucursal"] = idSucursal;
+            dataRow["NombreSucursal"] = nombre;
+            dataRow["DescripcionSucursal"] = descripcion;
+
+            dataTable.Rows.Add(dataRow);
+
+            return dataTable;
+        }
+
+
 
     }
 }
